2d325c64cbddf4fc1c8ad638ac41f29269912d8a
NeoEZ Devteam <dev@inblock.co>
2022-03-23 15:09:43 +0900

    Add Login

--- app/src/main/java/co/inblock/metawalletcallexample/LoginFragment.java
+++ app/src/main/java/co/inblock/metawalletcallexample/LoginFragment.java
@@ -14,10 +14,11 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import co.inblock.metawalletcallexample.databinding.FragmentFirstBinding;
+import co.inblock.metawalletcallexample.databinding.LoginFragmentBinding;
 
-public class FirstFragment extends Fragment {
-    private FragmentFirstBinding binding;
+
+public class LoginFragment extends Fragment {
+    private LoginFragmentBinding binding;
 
     ActivityResultLauncher<Intent>
             mStartForResult = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
@@ -29,15 +30,15 @@ public class FirstFragment extends Fragment {
                         return;
                     }
                     Bundle bundle = intent.getExtras();
-                    binding.txtResult.setText("RESULT_OK");
+                    binding.txtResult.setText(R.string.result_success);
                     binding.txtResultCode.setText(bundle.getString("CODE", ""));
                     binding.txtResultMessage.setText(bundle.getString("MESSAGE", ""));
-                    binding.txtResultTXID.setText(bundle.getString("TXID", ""));
+                    binding.txtResultAddress.setText(bundle.getString("DATA", ""));
                 } else if (i == Activity.RESULT_CANCELED) {
-                    binding.txtResult.setText("RESULT_CANCELED");
+                    binding.txtResult.setText(R.string.result_cancel);
                     binding.txtResultCode.setText("");
                     binding.txtResultMessage.setText("");
-                    binding.txtResultTXID.setText("");
+                    binding.txtResultAddress.setText("");
                 }
             });
 
@@ -48,10 +49,10 @@ public class FirstFragment extends Fragment {
 
     @Override
     public View onCreateView(
-            LayoutInflater inflater, ViewGroup container,
+            @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-        binding = FragmentFirstBinding.inflate(inflater, container, false);
+        binding = LoginFragmentBinding.inflate(inflater, container, false);
         return binding.getRoot();
     }
 
@@ -59,32 +60,44 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding.buttonFirst2.setOnClickListener(v -> {
+        binding.btnLogin.setOnClickListener(v -> {
             Uri params = Uri.parse("metawallet://co.inblock");
             Intent intent = new Intent(Intent.ACTION_VIEW, params);
             // intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
             intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
-            intent.putExtra("action", "send");
-            intent.putExtra("address", binding.edtReceiveAddress.getText().toString());
-            intent.putExtra("amount", binding.edtAmount.getText().toString());
+            intent.putExtra("action", "login");
+            intent.putExtra("callback", binding.edtCallback.getText().toString());
+            intent.putExtra("reqKey", binding.edtReqKey.getText().toString());
             if (binding.rdoMainnet.isChecked()) {
                 intent.putExtra("network", "1");
             } else {
                 intent.putExtra("network", "5");
             }
-            intent.putExtra("token", binding.edtToken.getText().toString());
 
-            // Option
-            intent.putExtra("tag", binding.edtTag.getText().toString());
-            intent.putExtra("memo", binding.edtMemo.getText().toString());
-            intent.putExtra("topic", binding.edtTopic.getText().toString());
+            intent.putExtra("callbackType", "intent");
+            if (binding.rdodeeplink.isChecked()) {
+                intent.putExtra("callbackType", "deeplink");
+            } else if (binding.rdoUrl.isChecked()) {
+                intent.putExtra("callbackType", "url");
+            }
+
+            intent.putExtra("name", binding.edtName.getText().toString());
+            intent.putExtra("icons", binding.edtIcons.getText().toString());
+
 
             binding.txtResult.setText("");
             binding.txtResultCode.setText("");
             binding.txtResultMessage.setText("");
-            binding.txtResultTXID.setText("");
+            binding.txtResultAddress.setText("");
 
-            mStartForResult.launch(intent);
+            try {
+                mStartForResult.launch(intent);
+            } catch (Exception e) {
+                binding.txtResult.setText(R.string.metawallet_not_found);
+                binding.txtResultCode.setText("");
+                binding.txtResultMessage.setText(e.getLocalizedMessage());
+                binding.txtResultAddress.setText("");
+            }
         });
     }
 
--- app/src/main/java/co/inblock/metawalletcallexample/MainActivity.java
+++ app/src/main/java/co/inblock/metawalletcallexample/MainActivity.java
@@ -3,21 +3,27 @@ package co.inblock.metawalletcallexample;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
+import androidx.navigation.NavHost;
 import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 import co.inblock.metawalletcallexample.databinding.ActivityMainBinding;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
+    private AppBarConfiguration mAppBarConfiguration;
     private ActivityMainBinding binding;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,12 +31,18 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        setSupportActionBar(binding.toolbar);
-
+        setSupportActionBar(binding.appBarMain.toolbar);
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_login, R.id.nav_transfer, R.id.nav_slideshow)
+                .setOpenableLayout(drawer)
+                .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 
     @Override
@@ -41,24 +53,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public boolean onSupportNavigateUp() {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
 }
